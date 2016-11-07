bfcb7bb00a11f052e7546b07223148c827ef7c3a
Raul Tierno <raultm@gmail.com>
2016-11-07 22:24:39 +0100

    Remove some code and define som rules

--- app/src/main/java/raulete/com/footballfield/custom/FootballFieldLayout.java
+++ app/src/main/java/raulete/com/footballfield/custom/FootballFieldLayout.java
@@ -14,18 +14,45 @@ import android.widget.RemoteViews;
  * can simplify it for more specific cases.
  *
  * https://developer.android.com/reference/android/view/ViewGroup.html
+ *
+ * onMeasure
+ * All the players are going to have the same dimensions.
+ *
+ *  ______________________
+ * |   D  M    |   M  D   |
+ * |   D  M  F | F M  D   |
+ * |G          |         G|
+ * |   D  M  F | F M  D   |
+ * |___D__M____|___M__D___|
+ *
+ *
+ * onLayout
+ *
+ * The position of a player always will be relative 0-100 from the field point of view to avoid
+ * problems when drawing th field in different resolutions
+ *
+ * 0           50        100
+ *  ______________________    0
+ * |           |          |
+ * |           |          |
+ * |           |          |
+ * |           |          |
+ * |___________|__________|  100
+ *
+ *
+ * The position of the player will be referenced to the middle of the PlayerViewItself. I don't know
+ * of it's gonna give problems this decision, but I need to define this now before use different
+ * approaches
+ *
+ *  ___________
+ * |           |
+ * |           |
+ * |     X     |
+ * |           |
+ * |___________|
  */
 @RemoteViews.RemoteView
 public class FootballFieldLayout extends ViewGroup {
-    /** The amount of space used by children in the left gutter. */
-    private int mLeftWidth;
-
-    /** The amount of space used by children in the right gutter. */
-    private int mRightWidth;
-
-    /** These are used for computing child frames based on their gravity. */
-    private final Rect mTmpContainerRect = new Rect();
-    private final Rect mTmpChildRect = new Rect();
 
     public FootballFieldLayout(Context context) {
         super(context);
@@ -51,61 +78,17 @@ public class FootballFieldLayout extends ViewGroup {
     /**
      * Ask all children to measure themselves and compute the measurement of this
      * layout based on the children.
+     *
+
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int count = getChildCount();
-
-        // These keep track of the space we are using on the left and right for
-        // views positioned there; we need member variables so we can also use
-        // these for layout later.
-        mLeftWidth = 0;
-        mRightWidth = 0;
-
-        // Measurement will ultimately be computing these values.
-        int maxHeight = 0;
-        int maxWidth = 0;
-        int childState = 0;
-
-        // Iterate through all children, measuring them and computing our dimensions
-        // from their size.
-        for (int i = 0; i < count; i++) {
-            final View child = getChildAt(i);
-            if (child.getVisibility() != GONE) {
-                // Measure the child.
-                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
-
-                // Update our size information based on the layout params.  Children
-                // that asked to be positioned on the left or right go in those gutters.
-                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
-                if (lp.position == LayoutParams.POSITION_LEFT) {
-                    mLeftWidth += Math.max(maxWidth,
-                            child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
-                } else if (lp.position == LayoutParams.POSITION_RIGHT) {
-                    mRightWidth += Math.max(maxWidth,
-                            child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
-                } else {
-                    maxWidth = Math.max(maxWidth,
-                            child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
-                }
-                maxHeight = Math.max(maxHeight,
-                        child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin);
-                childState = combineMeasuredStates(childState, child.getMeasuredState());
-            }
-        }
-
-        // Total width is the maximum width of all inner children plus the gutters.
-        maxWidth += mLeftWidth + mRightWidth;
-
-        // Check against our minimum height and width
-        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
-        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
-
-        // Report our final dimensions.
-        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
-                resolveSizeAndState(maxHeight, heightMeasureSpec,
-                        childState << MEASURED_HEIGHT_STATE_SHIFT));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
+
+    /** These are used for computing child frames based on their gravity. */
+    private final Rect mTmpContainerRect = new Rect();
+    private final Rect mTmpChildRect = new Rect();
 
     /**
      * Position all children within this layout.
@@ -117,10 +100,6 @@ public class FootballFieldLayout extends ViewGroup {
         // These are the far left and right edges in which we are performing layout.
         int leftPos = getPaddingLeft();
         int rightPos = right - left - getPaddingRight();
-
-        // This is the middle region inside of the gutter.
-        final int middleLeft = leftPos + mLeftWidth;
-        final int middleRight = rightPos - mRightWidth;
 
         // These are the top and bottom edges in which we are performing layout.
         final int parentTop = getPaddingTop();
@@ -143,9 +122,6 @@ public class FootballFieldLayout extends ViewGroup {
                     mTmpContainerRect.right = rightPos - lp.rightMargin;
                     mTmpContainerRect.left = rightPos - width - lp.leftMargin;
                     rightPos = mTmpContainerRect.left;
-                } else {
-                    mTmpContainerRect.left = middleLeft + lp.leftMargin;
-                    mTmpContainerRect.right = middleRight - lp.rightMargin;
                 }
                 mTmpContainerRect.top = parentTop + lp.topMargin;
                 mTmpContainerRect.bottom = parentBottom - lp.bottomMargin;
@@ -196,11 +172,10 @@ public class FootballFieldLayout extends ViewGroup {
          */
         public int gravity = Gravity.TOP | Gravity.START;
 
-        public static int POSITION_MIDDLE = 0;
         public static int POSITION_LEFT = 1;
         public static int POSITION_RIGHT = 2;
 
-        public int position = POSITION_MIDDLE;
+        public int position = POSITION_LEFT;
 
         public LayoutParams(Context c, AttributeSet attrs) {
             super(c, attrs);
